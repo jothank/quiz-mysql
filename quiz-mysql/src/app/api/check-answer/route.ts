dc9029e26847931c3e629c541bb01277033f23ac
Jonathan Costa <jothank@hotmail.com>
2024-11-21 10:54:30 -0300

    update

--- quiz-mysql/src/app/api/check-answer/route.ts
+++ quiz-mysql/src/app/api/check-answer/route.ts
@@ -7,14 +7,14 @@ export async function POST(req: Request) {
   const answers = {
     1: "SELECT * FROM pessoa;",
     2: "SELECT nome FROM clientes;",
-    // 3: "INSERT INTO produtos (nome, preco) VALUES ('nome_do_produto', valor_preco);",
-    // 4: "UPDATE produtos SET preco = 20 WHERE id = 5;",
-    // 5: "DELETE FROM clientes WHERE cidade = 'São Paulo';",
-    // 6: "SELECT * FROM produtos WHERE preco > 50;",
-    // 7: "SELECT COUNT(*) FROM clientes;",
-    // 8: "SELECT * FROM pessoa ORDER BY nome ASC;",
-    // 9: "SELECT DISTINCT cidade FROM clientes;",
-    // 10: "ALTER TABLE produtos RENAME COLUMN preco TO valor;",
+    3: "INSERT INTO produtos (nome, preco) VALUES ('nome_do_produto', valor_preco);",
+    4: "UPDATE produtos SET preco = 20 WHERE id = 5;",
+    5: "DELETE FROM clientes WHERE cidade = 'São Paulo';",
+    6: "SELECT * FROM produtos WHERE preco > 50;",
+    7: "SELECT COUNT(*) FROM clientes;",
+    8: "SELECT * FROM pessoa ORDER BY nome ASC;",
+    9: "SELECT DISTINCT cidade FROM clientes;",
+    10: "ALTER TABLE produtos RENAME COLUMN preco TO valor;",
   };
 
   const correctAnswer = answers[questionId];
